bd5bb917ba4ca00f05741f790d50095b65b5486d
Stella-Obiahu <stellapinky85@gmail.com>
2022-09-09 00:49:31 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Stella" ,
+    chips: 145
+}
 let cards = []
 let sum = 0
 let hasBlackjack = false
@@ -7,6 +11,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + " :$" + player.chips
 
 function getRandomCard(){
     let randomCard = Math.floor(Math.random() * 13) + 1
